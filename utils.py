df655b60541eea42a5f718d7dc8c983f3c3fb39d
dresson <edresson1@gmail.com>
2018-10-22 13:26:52 -0300

    update

--- utils.py
+++ utils.py
@@ -247,6 +247,7 @@ def text_normalize(text):
     
 def text_to_int_sequence(text):
     """ Use a character map and convert text to an integer sequence """
+    print('texto: ',text)
     int_sequence = []
     for c in text:
         if c == ' ':
