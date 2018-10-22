2295728935619c17f22946eb123e64d99f77a44d
dresson <edresson1@gmail.com>
2018-10-22 13:49:26 -0300

    update

--- data_generator.py
+++ data_generator.py
@@ -71,10 +71,10 @@ class DataGenerator(object):
         logger.info('Reading description file: {} for partition: {}'
                     .format(desc_file, partition))
         audio_paths, durations, texts, arpabets = [], [], [], []
-        with open(desc_file) as json_line_file:
+        with open(desc_file, encoding='utf-8') as json_line_file:
             for line_num, json_line in enumerate(json_line_file):
                 try:
-                    spec = json.loads(json_line,ensure_ascii=False)
+                    spec = json.loads(json_line)
                     if float(spec['duration']) > max_duration:
                         continue
                     audio_paths.append(spec['key'])
